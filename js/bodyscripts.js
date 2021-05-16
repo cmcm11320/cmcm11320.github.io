5da14ff5607cb1e648148a67bf370618145088df
cmcm11320 <cmcm11320@gmail.com>
2021-05-16 18:11:43 -0300

    Style changes

--- js/bodyscripts.js
+++ js/bodyscripts.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("navbar").style.width = "0";
   }
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("headmenu").style.top = "0";
+    } else {
+      document.getElementById("headmenu").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
