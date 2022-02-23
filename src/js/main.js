d1ede5ef06f17c45353ca518e62d1c58c7c38ed9
Clara Martins <cmcm11320@gmail.com>
2022-02-23 13:20:08 -0300

    design carroussel fixes

--- src/js/main.js
+++ src/js/main.js
@@ -69,3 +69,67 @@
     })
   }
 }())
+
+$(document).ready(function() {
+ 
+	var owl = $("#owl-demo");
+   
+	owl.owlCarousel({
+		items : 10, //10 items above 1000px browser width
+		itemsDesktop : [1000,5], //5 items between 1000px and 901px
+		itemsDesktopSmall : [900,3], // betweem 900px and 601px
+		itemsTablet: [600,2], //2 items between 600 and 0
+		itemsMobile : [600,1] // itemsMobile disabled - inherit from itemsTablet option
+	});
+   
+	// Custom Navigation Events
+	$(".next").click(function(){
+	  owl.trigger('owl.next');
+	})
+	$(".prev").click(function(){
+	  owl.trigger('owl.prev');
+	})
+	$(".play").click(function(){
+	  owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+	})
+	$(".stop").click(function(){
+	  owl.trigger('owl.stop');
+	})
+   
+  });
+
+  $(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+      autoPlay : 3000,
+      stopOnHover : true,
+      navigation:true,
+      paginationSpeed : 1000,
+      goToFirstSpeed : 2000,
+      singleItem : true,
+      autoHeight : true,
+      transitionStyle:"fade"
+    });
+   
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
